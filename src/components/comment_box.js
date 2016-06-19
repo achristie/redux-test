0630dacffa3ad59f665d54a2f5a05a1f972c8dba
Andrew Christie <awchristie86@gmail.com>
2016-06-18 23:39:53 -0400

    part 1

--- src/components/comment_box.js
+++ src/components/comment_box.js
@@ -1,14 +1,35 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 class CommentBox extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = { comment: '' };
+  }
+  
+  handleChange (event) {
+    this.setState({ comment: event.target.value });
+  }
+  
+  handleSubmit (event) {
+    event.preventDefault();
+    this.props.saveComment(this.state.comment);
+    console.log(this);
+    this.setState({ comment: '' });
+  }
+  
   render() {
     return (
-      <div>
-        <textarea />
-        <button>Submit Comment</button>
-      </div>
+      <form className='comment-box' onSubmit={this.handleSubmit.bind(this)}>
+        <textarea 
+          value={this.state.comment}
+          onChange={this.handleChange.bind(this)}/>
+        <button action='submit'>Submit Comment</button>
+      </form>
     );
   }
 }
 
-export default CommentBox;
+export default connect(null, actions)(CommentBox);
--- test/components/comment_box_test.js
+++ test/components/comment_box_test.js
@@ -5,7 +5,7 @@ describe('CommentBox', () => {
     let component;
     
     beforeEach(() => {
-      component = renderComponent(CommentBox);            
+      component = renderComponent(CommentBox);     
     });
   
     it('has a text area', () => {
@@ -15,5 +15,20 @@ describe('CommentBox', () => {
     it('has a button', () => {
       expect(component.find('button')).to.exist;
     });
+    
+    describe('entering some text', () => {
+      beforeEach(() => {
+        component.find('textarea').simulate('change', 'new comment');
+      });
+      
+      it('shows text in the text area', () => {
+        expect(component.find('textarea')).to.have.value('new comment');
+      });
+      
+      it('when submitted, clears the input', () => {
+        component.simulate('submit');
+        expect(component.find('textarea')).to.have.value('');
+      });
+    });    
 });
 
